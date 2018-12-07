d21e6a45e3cd09a1cb53960eae2f8904da0435ca
lmgttt <45025511+lmgttt@users.noreply.github.com>
2018-12-07 15:50:57 +0900

    Update MyFrame.java

--- JAVA_GUI_CODE/MyFrame.java
+++ JAVA_GUI_CODE/MyFrame.java
@@ -1,25 +1,34 @@
-package GUI;
+/**
+ * My frame class
+ */
 
 /**
- * 
+ * @version 1.0.1
+ * @author 2017315005 안시후, 201731502 최재성, 2017315035 김선혁, 2017315053 후쿠미쓰 치아키
  */
+
+package GUI;
 
 import javax.swing.JFrame;
 
 import Process.LibrarySystem;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame
+{
 
-	public MyFrame() {
+	public MyFrame()
+	{
 		this.setTitle("App for Librarian");
 		this.setSize(400, 430);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		LibrarySystem ourLib = new LibrarySystem("Team 1");
-		try{
+		try
+		{
 			ourLib.loadFile();
-		}catch(NullPointerException e){
-		
+		} catch (NullPointerException e)
+		{
+
 		}
 		this.add(new MyMainPanel(ourLib));
 
