0cc1b7c10a66e9272bccc843a26f853531b98270
SeonHyeokk <44061984+SeonHyeokk@users.noreply.github.com>
2018-12-07 18:45:12 +0900

    Update MyMainPanel.java

--- JAVA_GUI_CODE/MyMainPanel.java
+++ JAVA_GUI_CODE/MyMainPanel.java
@@ -3,7 +3,7 @@
  */
 
 /**
- * @version 1.0.1
+ * @version 1.0.2
  * @author 2017315005 안시후, 2017315027 최재성, 2017315035 김선혁, 2017315053 후쿠미쓰 치아키
  */
 
@@ -21,14 +21,12 @@ import javax.swing.JPanel;
 
 import Process.LibrarySystem;
 
-public class MyMainPanel extends JPanel
-{
+public class MyMainPanel extends JPanel {
 	JFrame newFrame;
 
-	public MyMainPanel(LibrarySystem ourLib)
-	{
+	public MyMainPanel(LibrarySystem ourLib) {
 
-		this.setSize(400, 430);
+		this.setSize(400, 700);
 
 		JLabel mainLabel = new JLabel("Welcom to " + ourLib.getName());
 		Font myFont = new Font("malgun gotic", Font.BOLD, 20);
@@ -38,8 +36,16 @@ public class MyMainPanel extends JPanel
 
 		JButton registerOneBorrowerBtn = new JButton("registerOneBorrower");
 		registerOneBorrowerBtn.setPreferredSize(new Dimension(220, 50));
+		JButton deleteOneBorrowerBtn = new JButton("deleteOneBorrower");
+		deleteOneBorrowerBtn.setPreferredSize(new Dimension(220, 50));
 		JButton addOneBookBtn = new JButton("addOneBook");
 		addOneBookBtn.setPreferredSize(new Dimension(220, 50));
+		JButton deleteOneBookBtn = new JButton("deleteOneBook");
+		deleteOneBookBtn.setPreferredSize(new Dimension(220, 50));
+		JButton searchBorrower = new JButton("searchBorrower");
+		searchBorrower.setPreferredSize(new Dimension(220, 50));
+		JButton searchBooks = new JButton("searchBooks");
+		searchBooks.setPreferredSize(new Dimension(220, 50));
 		JButton displayBooksAvailableForLoanBtn = new JButton("displayBooksAvailableForLoan");
 		displayBooksAvailableForLoanBtn.setPreferredSize(new Dimension(220, 50));
 		JButton displayBooksOnLoanBtn = new JButton("displayBooksOnLoan");
@@ -50,17 +56,19 @@ public class MyMainPanel extends JPanel
 		returnOneBookBtn.setPreferredSize(new Dimension(220, 50));
 
 		this.add(registerOneBorrowerBtn);
+		this.add(deleteOneBorrowerBtn);
 		this.add(addOneBookBtn);
+		this.add(deleteOneBookBtn);
+		this.add(searchBorrower);
+		this.add(searchBooks);
 		this.add(displayBooksAvailableForLoanBtn);
 		this.add(displayBooksOnLoanBtn);
 		this.add(lendOneBookBtn);
 		this.add(returnOneBookBtn);
 
-		registerOneBorrowerBtn.addActionListener(new ActionListener()
-		{
+		registerOneBorrowerBtn.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
+			public void actionPerformed(ActionEvent e) {
 				newFrame = new JFrame("registerOneBorrower");
 				newFrame.setSize(400, 220);
 				newFrame.add(new MyRegisterOneBorrowerPanel(newFrame, ourLib));
@@ -68,12 +76,22 @@ public class MyMainPanel extends JPanel
 				newFrame.setVisible(true);
 			}
 		});
-
-		addOneBookBtn.addActionListener(new ActionListener()
-		{
+		deleteOneBorrowerBtn.addActionListener(new ActionListener() {
+			
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				newFrame = new JFrame("registerOneBorrower");
+				newFrame.setSize(400, 220);
+				newFrame.add(new MyDeleteBorrowerInformationPanel(ourLib));
+
+				newFrame.setVisible(true);
+				
+			}
+		});
+		addOneBookBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
 				newFrame = new JFrame("addOneBook");
 				newFrame.setSize(400, 430);
 				newFrame.add(new MyAddOneBookPanel(newFrame, ourLib));
@@ -81,15 +99,46 @@ public class MyMainPanel extends JPanel
 				newFrame.setVisible(true);
 			}
 		});
+		deleteOneBookBtn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				newFrame = new JFrame("deleteOneBook");
+				newFrame.setSize(400, 220);
+				newFrame.add(new MyDeleteOneBookPanel(ourLib));
 
+				newFrame.setVisible(true);
+				
+			}
+		});
 		displayBooksAvailableForLoanBtn.addActionListener(new Display(ourLib, true));
 		displayBooksOnLoanBtn.addActionListener(new Display(ourLib, false));
-
-		lendOneBookBtn.addActionListener(new ActionListener()
-		{
+		searchBorrower.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
+			public void actionPerformed(ActionEvent e) {
+				JFrame frame = new JFrame();
+				frame.setTitle("SearchBorrower");
+				frame.setSize(700, 595);
+				SearchMain display = new SearchMain(ourLib, true);
+				frame.add(display);
+				frame.setVisible(true);
+			}
+		});
+		searchBooks.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame frame = new JFrame();
+				frame.setTitle("SerachBooks");
+				frame.setSize(700, 595);
+				SearchMain display = new SearchMain(ourLib, false);
+				frame.add(display);
+				frame.setVisible(true);
+			}
+		});
+		lendOneBookBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
 				newFrame = new JFrame("lendOneBook");
 				newFrame.setSize(400, 430);
 				newFrame.add(new MyLendOneBookPanel(newFrame, ourLib));
@@ -98,11 +147,9 @@ public class MyMainPanel extends JPanel
 			}
 		});
 
-		returnOneBookBtn.addActionListener(new ActionListener()
-		{
+		returnOneBookBtn.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
+			public void actionPerformed(ActionEvent e) {
 				newFrame = new JFrame("returnOneBook");
 				newFrame.setSize(400, 220);
 				newFrame.add(new MyReturnOneBookPanel(newFrame, ourLib));
