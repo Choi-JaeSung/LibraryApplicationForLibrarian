ded7e5fb102f7fe3c9bbd904ebfeaff804ef4932
lmgttt <45025511+lmgttt@users.noreply.github.com>
2018-12-07 15:55:27 +0900

    Update Display.java

--- JAVA_GUI_CODE/Display.java
+++ JAVA_GUI_CODE/Display.java
@@ -1,3 +1,12 @@
+/**
+ * Display implements ActionListener
+ */
+
+/**
+ * @version 1.0.1
+ * @author 2017315005 안시후, 2017315027 최재성, 2017315035 김선혁, 2017315053 후쿠미쓰 치아키
+ */
+
 package GUI;
 
 import java.awt.event.ActionEvent;
@@ -7,27 +16,32 @@ import javax.swing.JFrame;
 
 import Process.LibrarySystem;
 
-public class Display implements ActionListener {
+public class Display implements ActionListener
+{
 	LibrarySystem library;
 	boolean loan;
 
-	public Display(LibrarySystem library, boolean loan) {
+	public Display(LibrarySystem library, boolean loan)
+	{
 		// TODO Auto-generated constructor stub
 		this.library = library;
 		this.loan = loan;
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e)
+	{
 		// TODO Auto-generated method stub
-		if (loan == true) {
+		if (loan == true)
+		{
 			JFrame frame = new JFrame();
 			frame.setTitle("DisplayBooksAvailableForLoanBtn");
 			frame.setSize(700, 595);
 			MyDisplayBooksPanel display = new MyDisplayBooksPanel(library, true);
 			frame.add(display);
 			frame.setVisible(true);
-		} else {
+		} else
+		{
 			JFrame frame = new JFrame();
 			frame.setTitle("DisplayBooksOnLoanBtn");
 			frame.setSize(700, 595);
