ce2b265754f088e586594d2d2c6c0554b66741b5
lmgttt <45025511+lmgttt@users.noreply.github.com>
2018-12-07 15:48:51 +0900

    Update Comp.java

--- JAVA_CODE/Comp.java
+++ JAVA_CODE/Comp.java
@@ -1,12 +1,23 @@
+/**
+ * Comp class implements by Comparator<Book>
+ */
+
+/**
+ * @version 1.0.1
+ * @author 2017315005 안시후, 201731502 최재성, 2017315035 김선혁, 2017315053 후쿠미쓰 치아키
+ */
+
 package Process;
+
 import java.util.Comparator;
 
-public class Comp implements Comparator<Book>{
+public class Comp implements Comparator<Book>
+{
 
 	@Override
-	public int compare(Book o1, Book o2) {
-		// TODO Auto-generated method stub
-		return o1.getCatalogueNumber() < o2.getCatalogueNumber() ? - 1 : (o1 == o2 ? 0 : 1);
+	public int compare(Book o1, Book o2)
+	{
+		return o1.getCatalogueNumber() < o2.getCatalogueNumber() ? -1 : (o1 == o2 ? 0 : 1);
 	}
 
 }
