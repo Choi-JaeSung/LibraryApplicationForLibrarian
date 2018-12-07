cda8b7f061b98e0e01b6994f17c8f758bea7f831
lmgttt <45025511+lmgttt@users.noreply.github.com>
2018-12-07 15:54:08 +0900

    Update MyReturnOneBookPanel.java

--- JAVA_GUI_CODE/MyReturnOneBookPanel.java
+++ JAVA_GUI_CODE/MyReturnOneBookPanel.java
@@ -1,3 +1,12 @@
+/**
+ * Return one book panel class
+ */
+
+/**
+ * @version 1.0.1
+ * @author 2017315005 안시후, 201731502 최재성, 2017315035 김선혁, 2017315053 후쿠미쓰 치아키
+ */
+
 package GUI;
 
 import java.awt.Dimension;
