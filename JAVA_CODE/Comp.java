a2f6ae2944becec5ab91908ae299cce699454457
SeonHyeokk <44061984+SeonHyeokk@users.noreply.github.com>
2018-12-07 18:36:03 +0900

    Update Comp.java

--- JAVA_CODE/Comp.java
+++ JAVA_CODE/Comp.java
@@ -3,21 +3,19 @@
  */
 
 /**
- * @version 1.0.1
+ * @version 1.0.2
  * @author 2017315005 안시후, 2017315027 최재성, 2017315035 김선혁, 2017315053 후쿠미쓰 치아키
  */
 
 package Process;
-
 import java.util.Comparator;
 
-public class Comp implements Comparator<Book>
-{
+public class Comp implements Comparator<Book>{
 
 	@Override
-	public int compare(Book o1, Book o2)
-	{
-		return o1.getCatalogueNumber() < o2.getCatalogueNumber() ? -1 : (o1 == o2 ? 0 : 1);
+	public int compare(Book o1, Book o2) {
+		// TODO Auto-generated method stub
+		return o1.getCatalogueNumber() < o2.getCatalogueNumber() ? - 1 : (o1 == o2 ? 0 : 1);
 	}
 
 }
