0253a335f3d3bb00ee0dd427529abf73bd1d2a16
SeonHyeokk <44061984+SeonHyeokk@users.noreply.github.com>
2018-12-07 18:35:09 +0900

    Update Borrower.java

--- JAVA_CODE/Borrower.java
+++ JAVA_CODE/Borrower.java
@@ -3,7 +3,7 @@
  */
 
 /**
- * @version 1.0.1
+ * @version 1.0.2
  * @author 2017315005 안시후, 2017315027 최재성, 2017315035 김선혁, 2017315053 후쿠미쓰 치아키
  */
 
@@ -11,45 +11,36 @@ package Process;
 
 import java.util.Vector;
 
-public class Borrower
-{
+public class Borrower {
 	private String name;
 	private Vector<Book> books;
 
-	public Borrower(String name)
-	{
+	public Borrower(String name) {
 		this.name = name;
 		books = new Vector<Book>();
 	}
 
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
 
-	public Vector<Book> getBooks()
-	{
+	public Vector<Book> getBooks() {
 		return books;
 	}
 
-	public void setBook(Vector<Book> books)
-	{
+	public void setBook(Vector<Book> books) {
 		this.books = books;
 	}
 
-	public void detachBook(Book book)
-	{
-		for (int i = 0; i < books.size(); i++)
-		{
-			if (books.elementAt(i).getCatalogueNumber() == book.getCatalogueNumber())
-			{
+	public void detachBook(Book book) {
+		for (int i = 0; i < books.size(); i++) {
+			if (books.elementAt(i).getCatalogueNumber() == book.getCatalogueNumber()) {
 				books.remove(i);
 			}
 		}
 	}
 
-	public void attachBook(Book book)
-	{
+	public void attachBook(Book book) {
 		books.add(book);
 	}
 
