dd5ecd59734c52f7345cb8f78fa60c2167665eaf
SeonHyeokk <44061984+SeonHyeokk@users.noreply.github.com>
2018-12-07 18:39:30 +0900

    Update Display.java

--- JAVA_GUI_CODE/Display.java
+++ JAVA_GUI_CODE/Display.java
@@ -1,38 +1,49 @@
+/**
+ * Display implements ActionListener
+ */
+
+/**
+ * @version 1.0.2
+ * @author 2017315005 안시후, 2017315027 최재성, 2017315035 김선혁, 2017315053 후쿠미쓰 치아키
+ */
+
+package GUI;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 
-/**
- * 
- * @Author ()
- * @Version ()
- */
+import Process.LibrarySystem;
+
 public class Display implements ActionListener {
 	LibrarySystem library;
 	boolean loan;
 
 	public Display(LibrarySystem library, boolean loan) {
-		this.loan = loan;
+		// TODO Auto-generated constructor stub
 		this.library = library;
+		this.loan = loan;
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 		if (loan == true) {
 			JFrame frame = new JFrame();
 			frame.setTitle("DisplayBooksAvailableForLoanBtn");
 			frame.setSize(700, 595);
-			DisplayBooks display = new DisplayBooks(library, true);
+			MyDisplayBooksPanel display = new MyDisplayBooksPanel(library, loan);
 			frame.add(display);
 			frame.setVisible(true);
 		} else {
 			JFrame frame = new JFrame();
 			frame.setTitle("DisplayBooksOnLoanBtn");
 			frame.setSize(700, 595);
-			DisplayBooks display = new DisplayBooks(library, false);
+			MyDisplayBooksPanel display = new MyDisplayBooksPanel(library, loan);
 			frame.add(display);
 			frame.setVisible(true);
 		}
 	}
+
 }
